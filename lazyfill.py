3ff9f3fa14bb25e61c0d78dff62b757fa0c329da
kawangwong <nycazn724@gmail.com>
2021-09-15 18:07:33 -0400

    Add files via upload

---
+++ lazyfill.py
@@ -0,0 +1,27 @@
+import pdfrw
+pdfrw.__version__
+
+initial_pdf = "Generic.pdf"
+output_pdf = "filled.pdf"
+template_pdf = pdfrw.PdfReader(initial_pdf)
+
+ANNOT_KEY = '/Annots'
+ANNOT_FIELD_KEY = '/T'
+ANNOT_VAL_KEY = '/V'
+ANNOT_RECT_KEY = '/Rect'
+SUBTYPE_KEY = '/Subtype'
+WIDGET_SUBTYPE_KEY = '/Widget'
+
+
+columns = []
+
+for page in template_pdf.pages:
+    annotations = page[ANNOT_KEY]
+    for annotation in annotations:
+        if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
+            if annotation[ANNOT_FIELD_KEY]:
+                key = annotation[ANNOT_FIELD_KEY][1:-1]
+                columns.append(key)
+
+# print(columns)
+print(len(columns))
