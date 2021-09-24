b9c7228b92dc1d3445df4adae25c4603f66163c7
kawangwong <nycazn724@gmail.com>
2021-09-24 19:06:51 -0400

    Add files via upload

--- csvsetup.py
+++ csvsetup.py
@@ -1,11 +1,16 @@
 import csv
 from lazyfill import columns
 
-print(columns)
+# print(columns)
+print(columns[0])
+
+
+# item = columns[0]
+# print(item)
 
 with open("inputcsv.csv", "w") as f:
     writer = csv.writer(f)
-    writer.writerows(columns)
+    writer.writerows(zip(columns))
 
 print("complete")
 
